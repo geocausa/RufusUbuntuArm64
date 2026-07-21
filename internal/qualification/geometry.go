187fb58f6747f32a873df739939eaecff2dfef4e
geoca <geocausa@gmail.com>
2026-07-21 18:29:37 +0100

    Add checked creation-record extent arithmetic

---
+++ internal/qualification/geometry.go
@@ -0,0 +1,10 @@
+package qualification
+
+import "errors"
+
+func partitionExtentEnd(start, size uint64) (uint64, error) {
+	if size > ^uint64(0)-start {
+		return 0, errors.New("partition extent overflows byte geometry")
+	}
+	return start + size, nil
+}
