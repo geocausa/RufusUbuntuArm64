56cb8c2fd15fba7345ed523a86bfccbec4a515f0
geoca <geocausa@gmail.com>
2026-07-26 18:58:30 +0100

    trust: test ISO mastering allowlist

---
+++ internal/trustedexec/genisoimage_linux_test.go
@@ -0,0 +1,24 @@
+//go:build linux
+
+package trustedexec
+
+import (
+	"os"
+	"path/filepath"
+	"testing"
+)
+
+func TestResolveAtAcceptsTrustedGenISOImage(t *testing.T) {
+	root := t.TempDir()
+	path := filepath.Join(root, "genisoimage")
+	if err := os.WriteFile(path, []byte("#!/bin/sh\n"), 0o755); err != nil {
+		t.Fatal(err)
+	}
+	got, err := resolveAt("genisoimage", []string{root}, uint32(os.Getuid()))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if got != path {
+		t.Fatalf("got %s want %s", got, path)
+	}
+}
