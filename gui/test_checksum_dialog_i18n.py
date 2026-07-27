b3ddba952545a65b5a56284c49ccfac22c0cbebc
geoca <geocausa@gmail.com>
2026-07-27 14:29:38 +0100

    test: include USB qualification in exact secondary dialog set

--- gui/test_checksum_dialog_i18n.py
+++ gui/test_checksum_dialog_i18n.py
@@ -58,6 +58,7 @@ class ChecksumDialogLocalizationTests(unittest.TestCase):
             module.SECONDARY_DIALOG_CLASSES,
             (
                 ("rufusarm64_checksums", "ChecksumDialog"),
+                ("rufusarm64_device_qualify_dialog", "DeviceQualificationDialog"),
                 ("rufusarm64_nonbootable_dialog", "NonBootableFormatDialog"),
                 ("rufusarm64_freedos_dialog", "FreeDOSFormatDialog"),
             ),
